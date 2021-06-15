d23649f7afe52203ceebf02a33291256baf3fcc6
Dylan Schmittle <39482716+dylanschmittle@users.noreply.github.com>
2021-06-14 20:17:02 -0400

    Update text_extractor.py

--- text_extractor.py
+++ text_extractor.py
@@ -3,13 +3,16 @@ import wikipedia
 
 class TextExtractor:
 
-    __pageTitle: str
-    __pageId: str
+    __numberOfObjects: int
+    __csvObjects: [][]
 
     def __init__(self, pageTitle, pageId):
-        self.__pageTitle = pageTitle
-        self.__pageId = pageId
+        self.__numberOfObjects = 0
+        self.__csvObjects = [][]
 
+    def getObject(self, position=pos):
+        return self.__csvObjects[pos]
+        
     def extract(self):
         page = wikipedia.page(title=self.__pageTitle, pageid=self.__pageId)
         f = open("./text/" + self.__pageTitle + ".txt", "w")
@@ -17,5 +20,6 @@ class TextExtractor:
         f.close()
 
     def getText(self):
+        # TODO use csv class to import csv and save asd self.__csvObjects[][]
         f = open("./text/" + self.__pageTitle + ".txt", "r")
         return f.read()
